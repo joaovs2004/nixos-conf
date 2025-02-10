5eabd48f1e83f6f6af249a7744142d165566c1d8
joao.vs2004 <joaovitorschmittschmitt@gmail.com>
2025-02-10 16:18:36 -0300

    remove nvim directory

--- programs/nvim/lua/options.lua
+++
@@ -1,8 +0,0 @@
-require "nvchad.options"
-
--- add yours here!
-
-local o = vim.o
-vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#a0a0a0" })
--- o.cursorlineopt ='both' -- to enable cursorline!
-o.relativenumber = true
