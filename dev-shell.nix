5221c610f1ce4f869e7dde58a9029e6a5a0a873e
joao.vs2004 <joaovitorschmittschmitt@gmail.com>
2025-02-21 23:31:18 -0300

    Add dev shell

---
+++ dev-shell.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+(pkgs.buildFHSEnv {
+  name = "dev-shell";
+  targetPkgs = pkgs: (with pkgs; [
+    udev
+    alsa-lib
+    python3
+    nodejs
+    rustc
+    cargo
+    gcc
+  ]) ++ (with pkgs.xorg; [
+    libX11
+    libXcursor
+    libXrandr
+  ]);
+  multiPkgs = pkgs: (with pkgs; [
+    udev
+    alsa-lib
+  ]);
+  runScript = "zsh";
+}).env
